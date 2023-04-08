1fae07c577d05fee198be6ce15b76821dae27ad2
gianlucaguarini <gianluca.guarini@gmail.com>
2023-04-08 23:35:46 +0200

    updated: types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,10 @@
-declare const curri: <T extends (...args:unknown[]) => unknown, U>(...args:T[]) => U
+// types borrowed from https://stackoverflow.com/a/63905763
+type Curried<A extends unknown[], R> =
+  <P extends Partial<A>>(...args: P) => P extends A ? R :
+    A extends [...SameLength<P>, ...infer S] ? S extends unknown[] ? Curried<S, R>
+    : never : never;
+type SameLength<T extends unknown[]> = Extract<{ [K in keyof T]: unknown }, unknown[]>
+
+declare const curri: <A extends unknown[], R>(fn: (...args: A) => R) => Curried<A, R>
 
 export default curri
